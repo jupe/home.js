5f575d0a6bcfaa25d2046e4781367d2358c002f6
Jussi Vatjus-Anttila <jussiva@gmail.com>
2013-09-04 12:23:14 +0300

    update query library

--- app/lib/Query/lib/query.js
+++ app/lib/Query/lib/query.js
@@ -13,23 +13,19 @@ var parseQuery = function(query){
   /**
   [q=<query>][&c=true][&f=<fields>][&fo=true][&s=<order>][&sk=<skip>][&l=<limit>]
   q=<query> - restrict results by the specified JSON query
-  c=true - return the result count for this query
+  t= find|findOne|count|aggregate|distinct|populate..
   f=<set of fields> - specify the set of fields to include or exclude in each document (1 - include; 0 - exclude)
-  fo=true - return a single document from the result set (same as findOne() using the mongo shell
   s=<sort order> - specify the order in which to sort each specified field (1- ascending; -1 - descending)
-  p=<set of fields> - specify the set of fields to populate in each document
   sk=<num results to skip> - specify the number of results to skip in the result set; useful for paging
   l=<limit> - specify the limit for the number of results (default is 1000)
   */
   var qy = {
     q: {},      //  query
-    c: false,   //  count
+    t: 'find',   //  count
     f: '',      // fields
-    fo: false,  //  findOne / find
     s: {},      //  sort
     sk: 0,      //  skip
     l: 1000,     //  limit
-    p: false
   }
   
   var toJSON = function(str){
@@ -45,13 +41,11 @@ var parseQuery = function(query){
   for(var key in query){
     switch(key) {
       case('q'): qy.q = toJSON(query[key]); break;
-      case('c'): qy.c = query[key]=='true'?true:false; break;
+      case('t'): qy.t = query[key]; break;
       case('f'): qy.f = query[key]; break;
-      case('fo'): qy.fo = query[key]=='false'?false:true; break;
       case('s'): qy.s = toJSON(query[key]); break;
       case('sk'): qy.sk = parseInt(query[key]); break;
       case('l'): qy.l = parseInt(query[key]); break;
-      case('p'): qy.p = query[key]; break;
       default: 
         qy.q[key] = query[key];
         break;
@@ -65,22 +59,26 @@ var doQuery = function(query, model)
   if(!model)return query;
   if(dbg)console.log(query);
   var find = model;
-  if( query.f) find = find.select(query.f);
   
-  if( query.fo ) {
-    find = find.findOne(query.q);
-  } else {
-    find = find.find(query.q);
-  } 
-  if( query.c ){
-    find = find.count();
-    return find;
+  switch( query.t ){
+    case('find'): find = find.select(query.f); break;
+    case('findOne'): find = find.findOne(query.q); break;
+    case('count'): find = find.count(query.q); return find;
+    case('distinct'): 
+      find = find.distinct(query.f, query.q);
+      return find;
+    case('aggregate'): 
+      find = find.aggregate(query.q);
+    case('populate'): 
+      find = find.populate(query.q);
+  }
+  if( query.t != 'aggregate' ){
+    if( query.f) find = find.select(query.f);
   }
   
   if( query.l ) find = find.limit(query.l);
   if( query.sk ) find = find.skip(query.sk);
   if( query.s ) find = find.sort(query.s);
-  if( query.p) find = find.populate(query.q);
   
   return find;
 }
--- app/lib/Query/test/tests.js
+++ app/lib/Query/test/tests.js
@@ -8,27 +8,35 @@ describe('Query:basic', function() {
     var q = Query({q:'{}'});
     console.log(q);
     assert.equal( JSON.stringify(q.q), "{}"); //find()
-    assert.equal( q.c, false);                // !count
+    assert.equal( q.t, 'find');                // query type
     assert.equal( q.f, '');                   // select('')
-    assert.equal( q.fo, false);               // findOne | find
     assert.equal( JSON.stringify(q.s), "{}"); // sort()
     assert.equal( q.sk, 0);                   // skip(0)
     assert.equal( q.l, 1000);                 // limit(0)
-    assert.equal( q.p, false);                // populate instead of find
     done();
   });
   
   it('simple#2', function(done) {
-    var q = Query({q:'{}', l: 5, sk: 20, c: 'true'});
+    var q = Query({q:'{}', l: 5, sk: 20, t: 'count'});
+    console.log(q);
+    assert.equal( JSON.stringify(q.q), "{}");   //find()
+    assert.equal( q.t, 'count');                // !count
+    assert.equal( q.f, '');                     // select('')
+    assert.equal( JSON.stringify(q.s), "{}");   // sort()
+    assert.equal( q.sk, 20);                    // skip(20)
+    assert.equal( q.l, 5);                      // limit(5)
+    done();
+  });
+  
+  it('simple#3', function(done) {
+    var q = Query({q:'{}', l: 2, sk: 2, t: 'distinct'});
     console.log(q);
     assert.equal( JSON.stringify(q.q), "{}"); //find()
-    assert.equal( q.c, true);                // !count
+    assert.equal( q.t, 'distinct');           // distinct()
     assert.equal( q.f, '');                   // select('')
-    assert.equal( q.fo, false);               // findOne | find
     assert.equal( JSON.stringify(q.s), "{}"); // sort()
-    assert.equal( q.sk, 20);                   // skip(20)
-    assert.equal( q.l, 5);                 // limit(5)
-    assert.equal( q.p, false);                // populate instead of find
+    assert.equal( q.sk, 2);                   // skip(2)
+    assert.equal( q.l, 2);                    // limit(2)
     done();
   });
   
