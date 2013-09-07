b98fb090acb872f482f9f9a5655ff5b8e838a56e
Juhamatti Niemelä <iiska@iki.fi>
2013-09-07 10:41:31 +0300

    Use Angular UI bootstrap for layout / styling

--- public/js/services.js
+++ public/js/services.js
@@ -2,6 +2,7 @@
 
 /* Services */
 
+// TODO: Should configure root url for API calls in single config setting
 
 angular.module('homejs.services', ['ngResource']).
     factory('Device', function($resource) {
